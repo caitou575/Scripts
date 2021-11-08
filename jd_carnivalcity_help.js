8bc4b91c1cc73a94d3306c526cd90c940cc1748b
caitou575 <sk18661007200@gmail.com>
2021-11-08 21:19:00 +0800

    Update jd_carnivalcity_help.js

--- jd_carnivalcity_help.js
+++ jd_carnivalcity_help.js
@@ -285,7 +285,7 @@ function shareCodesFormat() {
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       $.newShareCodes = [...new Set([...$.shareCodes, ...$.updatePkActivityIdRes, ...(readShareCodeRes.data || [])])];
     } else {
-      $.newShareCodes = [...new Set([...$.shareCodes, ...$.updatePkActivityIdRes])];
+      $.newShareCodes = [...new Set([...$.shareCodes])];
     }
     console.log(`\n\n您将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
